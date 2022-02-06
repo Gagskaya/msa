7a0a22d595bf8247708795ec152034af188bb6cd
Gagskaya <alisxan006@mail.ru>
2022-02-06 21:14:35 +0300

    fixed dates

--- src/components/OrderCardHeader/index.tsx
+++ src/components/OrderCardHeader/index.tsx
@@ -14,32 +14,37 @@ interface OrderCardHeaderProps {
 };
 
 const OrderCardHeader: React.FC<OrderCardHeaderProps> = ({ order }) => {
-    const startMonth = order?.deliveries && format(new Date(order.deliveries[0].date), 'LLL', { locale: ru });
-    const startDay = order?.deliveries && format(new Date(order.deliveries[0].date), 'd', { locale: ru });
-    const endDay = order?.deliveries && format(new Date(order.deliveries[order.deliveries.length - 1].date), 'd', { locale: ru });
-    const endMonth = order?.deliveries && format(new Date(order.deliveries[order.deliveries.length - 1].date), 'LLL', { locale: ru });
-    const distance = order?.deliveries && formatDistance(new Date(order.deliveries[0].date), new Date(order.deliveries[order.deliveries.length - 1].date), { locale: ru });
+    if (order?.deliveries) {
+        const orderByDate = orderBy(order.deliveries, 'date', 'asc');
+        const startMonth = format(new Date(orderByDate[0].date), 'LLL', { locale: ru });
+        const startDay = format(new Date(orderByDate[0].date), 'd', { locale: ru });
+        const endDay = format(new Date(orderByDate[orderByDate.length - 1].date), 'd', { locale: ru });
+        const endMonth = format(new Date(orderByDate[orderByDate.length - 1].date), 'LLL', { locale: ru });
+        const distance = formatDistance(new Date(orderByDate[0].date), new Date(orderByDate[orderByDate.length - 1].date), { locale: ru });
 
-    return (
-        <div className="order-card__header">
-            <div className="order-card__header-title">
-                <h2>6 дней</h2>
-                <div>
-                    <span>{order?.packageName}</span>
-                    <h4>{order?.packageCalories}</h4>
-                </div>
+        return (
+            <div className="order-card__header">
+                <div className="order-card__header-title">
+                    <h2>6 дней</h2>
+                    <div>
+                        <span>{order?.packageName}</span>
+                        <h4>{order?.packageCalories}</h4>
+                    </div>
 
-            </div>
-            <div className="order-card__header-progress">
-                <Progress order={order} />
-                <div className="order-card__header-progress-footer">
-                    <p>{startMonth} {startDay}</p>
-                    <p>Осталось {`${distance}`}</p>
-                    <p>{endDay} {endMonth} </p>
+                </div>
+                <div className="order-card__header-progress">
+                    <Progress order={order} />
+                    <div className="order-card__header-progress-footer">
+                        <p>{startMonth} {startDay}</p>
+                        <p>Осталось {`${distance}`}</p>
+                        <p>{endDay} {endMonth} </p>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return null;
+    }
 };
 
 export default memo(OrderCardHeader);
